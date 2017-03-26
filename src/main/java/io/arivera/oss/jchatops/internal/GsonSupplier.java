077bfde78d3facd0c07a12999e4bb9956c95046f
Alejandro Rivera <alejandro.rivera.lopez@gmail.com>
2017-03-26 19:17:08 +0800

    Making GsonSupplier a Configuration

--- src/main/java/io/arivera/oss/jchatops/internal/GsonSupplier.java
+++ src/main/java/io/arivera/oss/jchatops/internal/GsonSupplier.java
@@ -2,15 +2,13 @@ package io.arivera.oss.jchatops.internal;
 
 import com.github.seratch.jslack.common.json.GsonFactory;
 import com.google.gson.Gson;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
 
 import java.util.function.Supplier;
 
-@Component
-@Scope("singleton")
+@Configuration
 public class GsonSupplier implements Supplier<Gson> {
 
   private final Gson gson;
@@ -20,7 +18,7 @@ public class GsonSupplier implements Supplier<Gson> {
   }
 
   @Bean
-  @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+  @Scope("singleton")
   public Gson get(){
     return gson;
   }
